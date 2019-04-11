42328f95f2f8013ff2bbf22fc95e4f7ffab8158a
santiagovasquezarias <santiagovasquezarias@gmail.com>
2019-04-11 08:33:20 -0500

    proyectos eliminados y nuevos agregados

---
+++ HolaMundo14/app/src/test/java/com/example/holamundo14/MainActivityTest.java
@@ -0,0 +1,32 @@
+package com.example.holamundo14;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("Hello World!".equals(appNameTextView.getText().toString()));
+    }
+
+}
